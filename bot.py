e4b3f8a2a4427c8efb4a988a53cce7b1e612eff1
nickraskop <raskop@usc.edu>
2021-08-01 01:40:44 -0700

    Added question function

--- bot.py
+++ bot.py
@@ -1,12 +1,33 @@
 import discord
+import requests
+import json
 
 client = discord.Client()
+
+
+def get_question():
+    qs = ''
+    id = 1
+    answer = 0
+    response = requests.get("http://127.0.0.1:8000/api/random/")
+    json_data = json.loads(response.text)
+    qs += "Question: \n"
+    qs += json_data[0]['title'] + '\n'
+
+    for item in json_data[0]['answer']:
+        qs += str(id) + ". " + item['answer'] + "\n"
+        if item['is_correct']:
+            answer = id
+        id += 1
+    return(qs, answer)
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello, I am a bot')
+    
+    if message.content.startswith('$question'):
+        question, answer = get_question()
+        await message.channel.send(question)
 
-client.run('ODcxMjIwNTQ4MjEzMDIyNzMy.YQYJXQ.cgf5AbCbzjph9p5ECotea2MMAXo')
+client.run('ODcxMjIwNTQ4MjEzMDIyNzMy.YQYJXQ.Vs7-DxZxEXfYG-UXg9NnkOsy3a0')
