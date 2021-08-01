c4685dd5ec5b3b3e04fb638369ba79fefaeb9583
nickraskop <raskop@usc.edu>
2021-07-31 21:32:02 -0700

    Added models and made migrations

--- bot.py
+++ bot.py
@@ -9,4 +9,4 @@ async def on_message(message):
     if message.content.startswith('hello'):
         await message.channel.send('Hello, I am a bot')
 
-client.run('ODcxMjIwNTQ4MjEzMDIyNzMy.YQYJXQ.sQL-9VW29qusVMpK0Sr1BZC1Wp8')
+client.run('ODcxMjIwNTQ4MjEzMDIyNzMy.YQYJXQ.cgf5AbCbzjph9p5ECotea2MMAXo')
