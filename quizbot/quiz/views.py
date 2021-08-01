bd350ed83b5ff60cf805c4be780e6d72e6555a9f
nickraskop <raskop@usc.edu>
2021-07-31 22:09:45 -0700

    Set up DRF

--- quizbot/quiz/views.py
+++ quizbot/quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Question
+from rest_framework.response import Response
+from .serializers import RandomQuestionSerializer
 
 # Create your views here.
+
+class RandomQuestion(APIView):
+
+    def get(self, request, format=None, **kwargs):
+        question = Question.objects.filter().order_by('?')[:1]
+        serializer = RandomQuestionSerializer(question, many=True)
+        return Response(serializer.data)
